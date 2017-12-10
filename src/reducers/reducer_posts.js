6642e009111b38d57a60bcc37e37877d4b8ba7c3
Frank Ruiz <frank.ruiz.limon@gmail.com>
2017-12-10 14:41:45 -0800

    Add ability to show single post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,15 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      // Commented partis ES5 way of doint it
+      // const post = action.payload.data;
+      // const newState = { ...state };
+      // newState[post.id] = post;
+      // return newState;
+      return { ...state, [action.payload.data.id]: action.payload.data }; //ES6
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
